d777c75d41b8b3a427d9f3c99e62a68f19cda743
Fleming Ho <flemingho@yahoo.com>
2023-12-10 17:57:16 -0500

    Initial commit

---
+++ MauiAndroidForegroundService/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MauiAndroidForegroundService;
+
+public partial class MainPage : FlyoutPage
+{
+    private readonly IMessenger _messenger;
+
+    public MainPage()
+	{
+        _messenger = IPlatformApplication.Current.Services.GetService<IMessenger>();
+        InitializeComponent();
+
+        //_messenger = messenger;
+    }
+
+    private void Start_OnClicked(object sender, EventArgs e)
+    {
+        _messenger.Send(new MessageData(Input.Text, true));
+    }
+
+    private void Stop_OnClicked(object sender, EventArgs e)
+    {
+        _messenger.Send(new MessageData(Input.Text, false));
+    }
+}
+
---
+++ MauiAndroidForegroundService/Page1.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAndroidForegroundService;
+
+public partial class Page1 : ContentPage
+{
+	public Page1()
+	{
+		InitializeComponent();
+	}
+}
